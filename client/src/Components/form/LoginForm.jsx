af959e446ab47aa2777bdc6bd793b5c8ff0f4a7d
yejinzz <yejin1320@naver.com>
2024-04-27 22:29:52 +0900

    refactor: 커스텀 훅 적용하여 유효성 로직 리팩토링

--- client/src/Components/form/LoginForm.jsx
+++ client/src/Components/form/LoginForm.jsx
@@ -1,91 +1,72 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import style from "./LoginForm.module.css";
-import Button from "@mui/material/Button";
 import axios from "axios";
 import { useNavigate } from "react-router";
 import { useSetRecoilState } from "recoil";
 import { loginState } from "../../store/auth";
-import OauthButton from "../Oauth/OauthButton";
+import Button from "../button/Button";
+import SignInput from "./SignInput";
+import useInputs from "../../hooks/useInputs";
 
 const LoginForm = () => {
-  const [loginInfo, setLoginInfo] = useState({
-    username: "",
-    password: "",
-  });
   const [errorMessage, setErrorMessage] = useState("");
   const setLoginState = useSetRecoilState(loginState);
   const navigate = useNavigate();
 
-  const handleInputValue = (key) => (e) => {
-    setLoginInfo({ ...loginInfo, [key]: e.target.value });
-  };
+  const { inputRef, form, validationMsg, handleValue } = useInputs({
+    username: "",
+    password: "",
+  });
 
-  const loginRequestHandler = () => {
-    if (!loginInfo.username || !loginInfo.password) {
-      setErrorMessage("아이디와 비밀번호를 입력하세요.");
-      return;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!form.username || !form.password) {
+      setErrorMessage("이메일과 비밀번호를 입력하세요.");
+    } else {
+      setErrorMessage("");
+      return axios
+        .post(
+          "http://ec2-3-34-211-22.ap-northeast-2.compute.amazonaws.com:8080/auth/login",
+          form
+        )
+        .then((res) => {
+          console.log(res);
+          if (res.status === 200) {
+            const authHeader = res.headers.authorization;
+            const token = authHeader.split(" ")[1];
+            const memberId = res.headers.memberid;
+            sessionStorage.setItem("token", token);
+            sessionStorage.setItem("id", memberId);
+            setLoginState(true);
+            navigate("/");
+          }
+        })
+        .catch((err) => {
+          console.log(err.response.data);
+          setErrorMessage("로그인에 실패했습니다.");
+        });
     }
-
-    return axios
-      .post(
-        "http://ec2-3-34-211-22.ap-northeast-2.compute.amazonaws.com:8080/auth/login",
-        loginInfo
-      )
-
-      .then((res) => {
-        console.log(res);
-
-        if (res.status === 200) {
-          const authHeader = res.headers.authorization;
-          const token = authHeader.split(" ")[1];
-          // axios.defaults.headers.common["authorization"] = `Bearer ${token}`;
-          const memberId = res.headers.memberid;
-          sessionStorage.setItem("token", token);
-          sessionStorage.setItem("id", memberId);
-          setLoginState(true);
-          setErrorMessage("");
-          navigate("/");
-        }
-      })
-      .catch((err) => {
-        console.log(err.response.data);
-        setErrorMessage("로그인에 실패했습니다.");
-      });
   };
 
   return (
-    <form className={style.loginForm} onSubmit={(e) => e.preventDefault()}>
-      <div className={style.fieldBox}>
-        <label>Email</label>
-        <input
-          className={style.input}
-          type="email"
-          onChange={handleInputValue("username")}
-        />
-      </div>
-      <div className={style.fieldBox}>
-        <label>Password</label>
-        <input
-          className={style.input}
-          type="password"
-          onChange={handleInputValue("password")}
-        />
-      </div>
-      <Button
-        //   className={style.button}
-        variant="contained"
-        sx={{ fontSize: 14, width: "100%", height: "40px" }}
-        onClick={loginRequestHandler}
-      >
-        Log in
-      </Button>
-      {errorMessage ? (
-        <div id="alert-message" data-testid="alert-message">
-          {errorMessage}
-        </div>
-      ) : (
-        ""
-      )}
+    <form className={style.loginForm} onSubmit={handleSubmit}>
+      <SignInput
+        type="email"
+        name="username"
+        onChange={handleValue}
+        errMsg={validationMsg.email}
+        inputRef={(el) => (inputRef.current[0] = el)}
+      />
+      <SignInput
+        type="password"
+        name="password"
+        onChange={handleValue}
+        errMsg={validationMsg.password}
+        inputRef={(el) => (inputRef.current[1] = el)}
+      />
+      {errorMessage && <p className={style.errMsg}>{errorMessage}</p>}
+      <Button type="submit">Log in</Button>
     </form>
   );
 };
--- client/src/Components/form/SignupForm.jsx
+++ client/src/Components/form/SignupForm.jsx
@@ -1,47 +1,74 @@
-import { useRef, useState } from "react";
 import style from "./SignupForm.module.css";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Button } from "@mui/material";
+import Button from "../button/Button";
+import useInputs from "../../hooks/useInputs";
+import SignInput from "./SignInput";
 
 const SignupForm = () => {
-  const [memberData, setMemberData] = useState({
-    email: "",
-    password: "",
-    displayName: "",
-  });
-  const [emailErrMsg, setEmailErrMsg] = useState("");
-  const [pwdErrMsg, setPwdErrMsg] = useState("");
-  const [nameErrMsg, setNameErrMsg] = useState("");
-
-  const emailValRef = useRef(null);
-  const pwdValRef = useRef(null);
-  const nameValRef = useRef(null);
-
-  const regExpPwd =
-    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-
   const navigate = useNavigate();
 
-  const handleEmailValue = () => {
-    let emailValue = emailValRef.current.value;
-    setMemberData({ ...memberData, email: emailValue });
-  };
-  const handlePwdValue = () => {
-    let pwdValue = pwdValRef.current.value;
-    setMemberData({ ...memberData, password: pwdValue });
-  };
-  const handleNameValue = () => {
-    let nameValue = nameValRef.current.value;
-    setMemberData({ ...memberData, displayName: nameValue });
+  const { inputRef, form, validationMsg, handleValidationMsg, handleValue } =
+    useInputs({
+      displayName: "",
+      email: "",
+      password: "",
+    });
+
+  // 필드 입력값 체크
+  const checkDisplayName = () => {
+    if (!form.displayName) {
+      handleValidationMsg("displayName", "❗️ 닉네임을 입력해주세요.");
+      return false;
+    } else {
+      handleValidationMsg("displayName", "");
+      return true;
+    }
   };
 
-  const postData = () => {
-    if (!emailErrMsg && !pwdErrMsg && !nameErrMsg) {
+  const checkEmail = () => {
+    if (!form.email) {
+      handleValidationMsg("email", "❗️ 이메일을 입력해주세요.");
+      return false;
+    } else {
+      handleValidationMsg("email", "");
+      return true;
+    }
+  };
+
+  const checkPassword = () => {
+    const regExpPwd =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+    if (!form.password) {
+      handleValidationMsg("password", "❗️ 비밀번호를 입력해주세요.");
+      return false;
+    } else if (!regExpPwd.test(form.password)) {
+      handleValidationMsg(
+        "password",
+        "❗️ 최소 8자, 하나의 이상의 대소문자, 숫자, 특수문자를 포함해야 합니다."
+      );
+      return false;
+    } else {
+      handleValidationMsg("password", "");
+      return true;
+    }
+  };
+
+  // form 제출
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    checkDisplayName();
+    checkEmail();
+    checkPassword();
+
+    const validation = checkDisplayName() && checkEmail() && checkPassword();
+
+    if (validation) {
       axios
         .post(
           "http://ec2-3-34-211-22.ap-northeast-2.compute.amazonaws.com:8080/members",
-          { ...memberData }
+          { ...form }
         )
         .then((res) => {
           console.log(res);
@@ -50,73 +77,39 @@ const SignupForm = () => {
         .catch((err) => {
           console.log(err);
           if (err.response.status === 409) {
-            setEmailErrMsg("❗️ 이미 사용중인 이메일 입니다.");
+            handleValidationMsg("email", "❗️ 이미 사용중인 이메일 입니다.");
           }
         });
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    handleEmailValue();
-    handlePwdValue();
-    handleNameValue();
-
-    if (memberData.email === "") {
-      setEmailErrMsg("❗️ 이메일을 입력해주세요.");
-    } else {
-      setEmailErrMsg("");
-    }
-
-    if (memberData.password === "") {
-      setPwdErrMsg("❗️ 비밀번호를 입력해주세요.");
-    } else if (!regExpPwd.test(memberData.password)) {
-      setPwdErrMsg(
-        "❗️ 최소 8자, 하나의 이상의 대소문자, 숫자, 특수문자를 포함해야 합니다."
-      );
-    } else {
-      setPwdErrMsg("");
-    }
-
-    if (memberData.displayName === "") {
-      setNameErrMsg("❗️ 닉네임을 입력해주세요.");
-    } else {
-      setNameErrMsg("");
-    }
-
-    return postData();
-  };
-
   return (
-    <form className={style.signupForm}>
-      <div className={style.fieldBox}>
-        <label className={style.title}>Display Name</label>
-        <input type="text" onChange={handleNameValue} ref={nameValRef} />
-        {nameErrMsg !== "" && <p className={style.errMsg}>{nameErrMsg}</p>}
-      </div>
-      <div className={style.fieldBox}>
-        <label className={style.title}>Email</label>
-        <input type="email" onChange={handleEmailValue} ref={emailValRef} />
-        {emailErrMsg !== "" && (
-          <div className={style.errMsg}>{emailErrMsg}</div>
-        )}
-      </div>
-      <div className={style.fieldBox}>
-        <label className={style.title}>Password</label>
-        <input type="password" onChange={handlePwdValue} ref={pwdValRef} />
-        {pwdErrMsg !== "" && <div className={style.errMsg}>{pwdErrMsg}</div>}
-      </div>
+    <form className={style.signupForm} onSubmit={handleSubmit}>
+      <SignInput
+        type="text"
+        name="displayName"
+        onChange={handleValue}
+        inputRef={(el) => (inputRef.current[0] = el)}
+        errMsg={validationMsg.displayName}
+      />
 
-      <Button
-        className={style.button}
-        type="submit"
-        onClick={handleSubmit}
-        variant="contained"
-        sx={{ fontSize: 14, width: "100%", height: "40px" }}
-      >
-        Sign Up
-      </Button>
+      <SignInput
+        type="email"
+        name="email"
+        onChange={handleValue}
+        inputRef={(el) => (inputRef.current[1] = el)}
+        errMsg={validationMsg.email}
+      />
+
+      <SignInput
+        type="password"
+        name="password"
+        onChange={handleValue}
+        inputRef={(el) => (inputRef.current[2] = el)}
+        errMsg={validationMsg.password}
+      />
+
+      <Button type="submit">Sign Up</Button>
       <p className={style.note}>
         By clicking “Sign up”, you agree to our terms of service and acknowledge
         that you have read and understand our privacy policy and code of
